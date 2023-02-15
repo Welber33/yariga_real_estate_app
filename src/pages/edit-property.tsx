91fbd423bc6a596905fb823dbbcb3ddfcbb820b7
Welber33 <jwwork21@gmail.com>
2023-02-15 15:39:01 -0300

    Refine Yariga Real Estate

---
+++ src/pages/edit-property.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function editProperty() {
+  return (
+    <div>edit-property</div>
+  )
+}
---
+++ src/pages/property-details.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function propertyDetails() {
+  return (
+    <div>property-details</div>
+  )
+}

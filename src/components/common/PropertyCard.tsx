9e86e445360c289797aa90656baa62f2ef7b6dfc
Welber33 <jwwork21@gmail.com>
2023-03-02 11:46:56 -0300

    Yariga Displaying Properties

--- src/components/common/PropertyCard.tsx
+++ src/components/common/PropertyCard.tsx
@@ -1,5 +1,56 @@
-export default function PropertyCard() {
+import { Place } from "@mui/icons-material"
+import { Link } from "@pankod/refine-react-router-v6"
+import { Typography, Box, Card, CardMedia, CardContent, Stack } from "@pankod/refine-mui"
+import { PropertyCardProps } from "interfaces/property"
+
+export default function PropertyCard({ id, title, location, price, photo }: PropertyCardProps) {
   return (
-    <div>PropertyCard</div>
+    <Card
+      component={Link}
+      to={`/properties/${id}`}
+      sx={{
+        maxWidth: "330px",
+        padding: "10px",
+        "&:hover": {
+          boxShadow: "0 22px 45px 2px rgba(176, 176, 176, 0.1)"
+        },
+        cursor: "pointer",
+        textDecoration: "none"
+      }}
+      elevation={0}
+    >
+      <CardMedia
+        component="img"
+        width="100%"
+        height={210}
+        image={photo}
+        alt="card image"
+        sx={{ borderRadius: "10px" }}
+      />
+
+      <CardContent
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          gap: "10px",
+          px: "5px"
+        }}
+      >
+        <Stack direction="column" gap={1}>
+          <Typography fontSize={18} fontWeight={500} color="#11142d">{title}</Typography>
+          <Stack direction="row" gap={0.5} alignItems="flex-start">
+            <Place
+              sx={{ fontSize: 18, color: "#8257e5", marginTop: 0.5 }}
+            />
+            <Typography fontSize={16} color="#808191">{location}</Typography>
+          </Stack>
+        </Stack>
+
+        <Box px={1.5} py={0.5} borderRadius={1} bgcolor="#e5ddf5" height="fit-content">
+          <Typography fontSize={14} fontWeight={600} color="#8257e5">${price}</Typography>
+        </Box>
+      </CardContent>
+    </Card>
   )
 }
--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -1,5 +1,5 @@
 import { Add } from "@mui/icons-material"
-import { useList } from "@pankod/refine-core"
+import { useTable } from "@pankod/refine-core"
 import { Box, Stack, Typography } from "@pankod/refine-mui"
 import { useNavigate } from "@pankod/refine-react-router-v6"
 
@@ -8,13 +8,20 @@ import { PropertyCard, CustomButton } from "components"
 export default function AllProperties() {
   const navigate = useNavigate()
 
+  const { tableQueryResult: { data, isLoading, isError } } = useTable();
+
+  const allProperties = data?.data ?? [];
+
+  if (isLoading) return <Typography>Loading...</Typography>
+  if (isError) return <Typography>Error...</Typography>
+
   return (
     <Box>
       <Stack direction="row" justifyContent="space-between" alignItems="center">
         <Typography fontSize={25} fontWeight={700} color="#11142d">
           All Properties
         </Typography>
-        <CustomButton 
+        <CustomButton
           title="Add Property"
           handleClick={() => navigate("/properties/create")}
           backgroundColor="#8257e5"
@@ -22,6 +29,19 @@ export default function AllProperties() {
           icon={<Add />}
         />
       </Stack>
+
+      <Box mt="20px" sx={{ display: "flex", flexWrap: "wrap", gap: 3 }}>
+        {allProperties.map((property) => (
+          <PropertyCard 
+            key={property._id}
+            id={property._id}
+            title={property.title}
+            price={property.price}
+            location={property.location}
+            photo={property.photo}
+          />
+        ))}
+      </Box>
     </Box>
   )
 }

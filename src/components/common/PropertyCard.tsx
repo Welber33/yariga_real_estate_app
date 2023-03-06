987a84fdbb26e4556d32e671318e2e57968b1ad5
Welber33 <jwwork21@gmail.com>
2023-03-06 13:02:39 -0300

    Yariga Property Details Page

--- src/components/common/PropertyCard.tsx
+++ src/components/common/PropertyCard.tsx
@@ -7,7 +7,7 @@ export default function PropertyCard({ id, title, location, price, photo }: Prop
   return (
     <Card
       component={Link}
-      to={`/properties/${id}`}
+      to={`/properties/show/${id}`}
       sx={{
         maxWidth: "330px",
         padding: "10px",
@@ -15,7 +15,6 @@ export default function PropertyCard({ id, title, location, price, photo }: Prop
           boxShadow: "0 22px 45px 2px rgba(176, 176, 176, 0.1)"
         },
         cursor: "pointer",
-        textDecoration: "none"
       }}
       elevation={0}
     >
--- src/pages/edit-property.tsx
+++ src/pages/edit-property.tsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import { useState } from "react";
+import { useGetIdentity } from "@pankod/refine-core"
+import { FieldValues, useForm } from "@pankod/refine-react-hook-form"
+import Form from "components/common/Form"
 
-export default function editProperty() {
+export default function EditProperty() {
+  const { data: user } = useGetIdentity();
+  const [propertyImage, setPropertyImage] = useState({ name: '', url: '' })
+  const { refineCore: { onFinish, formLoading }, register, handleSubmit } = useForm();
+
+  function handleImageChange(file: File) {
+    const reader = (readFile: File) => new Promise<string>((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.onload = () => resolve(fileReader.result as string);
+      fileReader.readAsDataURL(readFile);
+    })
+
+    reader(file)
+      .then((result: string) => setPropertyImage({ name: file?.name, url: result }));
+  }
+
+  async function onFinishHandler(data: FieldValues) {
+    if (!propertyImage.name) return alert("Please select an image");
+
+
+    await onFinish({
+      ...data,
+      photo: propertyImage.url,
+      email: user.email
+    })
+  }
+
   return (
-    <div>edit-property</div>
+    <Form
+      type="Edit"
+      register={register}
+      onFinish={onFinish}
+      formLoading={formLoading}
+      handleSubmit={handleSubmit}
+      handleImageChange={handleImageChange}
+      onFinishHandler={onFinishHandler}
+      propertyImage={propertyImage}
+    />
   )
 }
